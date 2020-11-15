03b5d664f5fd691f34cc4d4599e13d8d0c32ba3b
Ruben <rubenfricke@gmail.com>
2020-11-15 14:08:05 +0100

    Added Simple Equation challenge

---
+++ Simple Equation tests/Class1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Simple_Equation;
+
+namespace Simple_Equation_tests
+{
+    [TestFixture]
+    public class EquationTest
+    {
+
+        [Test]
+        public void TestPlus()
+        {
+            StringAssert.AreEqualIgnoringCase("x = 17", Kata.SolveTheEquation("x + 85 = 102"),
+                "Plus operator test failed");
+        }
+
+        [Test]
+        public void TestMultiply()
+        {
+            StringAssert.AreEqualIgnoringCase("x = 68.15", Kata.SolveTheEquation("x * 1358 = 92548"),
+                "Multiply operator test failed");
+        }
+
+        [Test]
+        public void TestMinus()
+        {
+            StringAssert.AreEqualIgnoringCase("x = 6553", Kata.SolveTheEquation("x - 1003 = 5550"),
+                "Minus operator test failed");
+        }
+
+        [Test]
+        public void TestDivide()
+        {
+            StringAssert.AreEqualIgnoringCase("x = 63250", Kata.SolveTheEquation("x / 5 = 12650"),
+                "Divide operator test failed");
+        }
+
+    }
+}
---
+++ Simple Equation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Simple_Equation
+{
+    public class Kata
+    {
+        public static void Main(string[] args) { }
+
+        public static string SolveTheEquation(string equation)
+        {
+            var equationParts = equation.Split(' ');
+            long a = Convert.ToInt64(equationParts[2]);
+            long b = Convert.ToInt64(equationParts[4]);
+
+            switch (equation[2])
+            {
+                case '/':
+                    return $"x = {a * b}";
+                case '*':
+                    return $"x = {(double)b / a:0.00}";
+                case '+':
+                    return $"x = {b - a}";
+            }
+
+            return $"x = {b + a}";
+        }
+    }
+}
